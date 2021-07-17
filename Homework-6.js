65cf1969e041c772eeb09fab884ffddacfb156cc
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-16 21:14:06 -0400

    carosel

--- Homework-6.js
+++ Homework-6.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, options);
+//   });
 
  
 
-  $enable-shadows
+//   $enable-shadows
 
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
 
